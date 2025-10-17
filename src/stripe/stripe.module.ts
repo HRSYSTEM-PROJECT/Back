f91acd9e5a967b1726ee3264f984576b28fee00c
Jesus May Iuit <jmi_1416@hotmail.com>
2025-10-17 17:00:13 -0600

    Importacion de modulos en Stripe

--- src/stripe/stripe.module.ts
+++ src/stripe/stripe.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { StripeService } from './stripe.service';
 import { StripeController } from './stripe.controller';
+import { UserModule } from 'src/user/user.module';
+import { Company } from 'src/empresa/entities/empresa.entity';
+import { Plan } from 'src/plan/entities/plan.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Company, Plan]), UserModule],
   controllers: [StripeController],
-  providers: [StripeService],
+  providers: [StripeService]
 })
 export class StripeModule {}
