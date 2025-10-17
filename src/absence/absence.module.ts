4110f83329bb144d87116dc1abc12fc9785e01a8
Jesus May Iuit <jmi_1416@hotmail.com>
2025-10-17 17:30:16 -0600

    Error de importacion en absence module

--- src/absence/absence.module.ts
+++ src/absence/absence.module.ts
@@ -5,11 +5,13 @@ import { AbsenceController } from './absence.controller';
 import { Absence } from './entities/absence.entity';
 import { Employee } from '../empleado/entities/empleado.entity';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Absence, Employee]),
-    NotificationsModule
+    NotificationsModule,
+    UserModule
   ],
   controllers: [AbsenceController],
   providers: [AbsenceService],
