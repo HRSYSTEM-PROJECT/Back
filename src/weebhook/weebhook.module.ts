c0755a3806a143138f3ce384dbc999be1a6cfa4c
Jesus May Iuit <jmi_1416@hotmail.com>
2025-10-17 17:04:57 -0600

    Importacion de modulos en Webhook

--- src/weebhook/weebhook.module.ts
+++ src/weebhook/weebhook.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { WebhookService } from './weebhook.service';
 import { WebhookController } from './weebhook.controller';
 import { SuscripcionService } from 'src/suscripcion/suscripcion.service';
+import { Company } from 'src/empresa/entities/empresa.entity';
+import { Plan } from 'src/plan/entities/plan.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Company, Plan]), UserModule],
   controllers: [WebhookController],
   providers: [WebhookService, SuscripcionService]
 })
