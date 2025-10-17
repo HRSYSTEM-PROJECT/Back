ecfb72bd94c045c74e285ea864f980fe63d31c46
Jesus May Iuit <jmi_1416@hotmail.com>
2025-10-17 17:19:08 -0600

    Solucion error importacion de modulos

--- src/weebhook/weebhook.module.ts
+++ src/weebhook/weebhook.module.ts
@@ -5,12 +5,13 @@ import { WebhookController } from './weebhook.controller';
 import { SuscripcionService } from 'src/suscripcion/suscripcion.service';
 import { Company } from 'src/empresa/entities/empresa.entity';
 import { Plan } from 'src/plan/entities/plan.entity';
+import { Suscripcion } from 'src/suscripcion/entities/suscripcion.entity';
 import { UserModule } from 'src/user/user.module';
 import { SuscripcionModule } from 'src/suscripcion/suscripcion.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Company, Plan]),
+    TypeOrmModule.forFeature([Company, Plan, Suscripcion]),
     UserModule,
     SuscripcionModule
   ],
