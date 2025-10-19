73102d2a5a0b05cc5d7860db90622f5ecd42f434
pablo putin <pablopuntin@gmail.com>
2025-10-18 23:01:30 -0300

    modificacion modulo position y departmento1

--- src/empresa/empresa.module.ts
+++ src/empresa/empresa.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Company } from './entities/empresa.entity';
 import { EmpresaService } from './empresa.service';
 import { EmpresaController } from './empresa.controller';
+import { DepartamentoModule } from 'src/departamento/departamento.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Company])],
+  imports: [TypeOrmModule.forFeature([Company, DepartamentoModule])],
   controllers: [EmpresaController],
   providers: [EmpresaService]
 })
